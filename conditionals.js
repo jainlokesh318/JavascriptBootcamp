6eb02db959aecfb1f68c2ca1dfe4dbbb0c49a619
Lokesh Jain <lokesh.jain@juspay.in>
2023-03-11 17:26:38 +0530

    if/else revision exercise

--- conditionals.js
+++ conditionals.js
@@ -1,1 +1,15 @@
-console.log("conditionals")
+//11 March 2023
+
+//Exercise 1 (Revision of if else conditional)
+let age = 17;
+
+// < 17 years -> discount 10%
+// 17 years age -> discount 17%
+// > 17 years -> discount 15%
+
+
+//2 
+//5
+//5 -> won the game
+
+//6 -> Out of game
