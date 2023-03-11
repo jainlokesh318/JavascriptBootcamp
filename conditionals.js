5cf5c8bee51f19bcaf2a7ba8e814ff7c74d04867
Lokesh Jain <lokesh.jain@juspay.in>
2023-03-11 17:32:17 +0530

    if/else revision exercise solution

--- conditionals.js
+++ conditionals.js
@@ -3,6 +3,14 @@
 //Exercise 1 (Revision of if else conditional)
 let age = 17;
 
+if (age < 17) {
+  console.log("discount 10%");
+} else if (age === 17) {
+  console.log("discount 17%");
+} else if (age > 17) {
+  console.log("discount 15%");
+}
+
 // < 17 years -> discount 10%
 // 17 years age -> discount 17%
 // > 17 years -> discount 15%
