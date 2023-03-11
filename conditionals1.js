a3b100bcceaee517623331af98c3fa328761bc78
Lokesh Jain <lokesh.jain@juspay.in>
2023-03-11 18:01:38 +0530

    dice game 2 check if wonTheGame

--- conditionals1.js
+++ conditionals1.js
@@ -17,11 +17,22 @@ let dice2 = 10;
 //4. if sum exceeds 12 -> "Bye, Bye! Better Luck Next Time."
 
 let sumOfDice = dice1 + dice2;
-//1. calculate sum in a new variable, sumOfDice
+
+let wonTheGame = false;
+
+//deciding section
 if (sumOfDice < 12) {
   console.log("Would you like to roll one more dice?");
 } else if (sumOfDice === 12) {
   console.log("Hurrah! you have won the game");
+  wonTheGame = true;
 } else {
   console.log("Bye, Bye! Better Luck Next Time");
+}
+
+//prize distribution
+if(wonTheGame){
+  console.log("Here is your prize")
+}else{
+  console.log("No Prize for you at this moment")
 }
