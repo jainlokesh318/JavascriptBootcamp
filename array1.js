0bdc8fa604b75501ccb28aba90f0d06387b15720
Lokesh Jain <lokesh.jain@juspay.in>
2023-03-12 18:06:38 +0530

    array Exercise 1 added

--- array1.js
+++ array1.js
@@ -10,3 +10,9 @@ console.log(list[1]);
 
 //length of an array
 console.log(list.length)
+
+// Exercise 1
+//1. Build a list of flowers with atleast 5 flowers in the list
+//2. Print the whole list
+//3. Print the list items seperately
+//4. Print the length of the list
