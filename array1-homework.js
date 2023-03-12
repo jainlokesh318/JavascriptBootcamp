c768fe10251e623b3223c5752e5c753afab24bb4
Lokesh Jain <lokesh.jain@juspay.in>
2023-03-12 18:56:54 +0530

    array take-home assignment added

---
+++ array1-homework.js
@@ -0,0 +1,9 @@
+// 1. Create a array with 5 strings in it and print its contents
+
+// 2. Add a new element to the existing array and print its contents.
+
+// 3. Remove last two elements of the existing array and print its contents
+
+// 4. Add "hello" to the array and print only even index places
+
+// 4. Add "heyyaaa" to the array and print only odd index places
