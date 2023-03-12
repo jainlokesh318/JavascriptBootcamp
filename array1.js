002432f14b9c42f92fe928494bc0058632270d33
Lokesh Jain <lokesh.jain@juspay.in>
2023-03-12 18:12:44 +0530

    array Exercise 1 solution

--- array1.js
+++ array1.js
@@ -16,3 +16,24 @@ console.log(list.length)
 //2. Print the whole list
 //3. Print the list items seperately
 //4. Print the length of the list
+let flowerlist = [
+    "Lily",
+    "Rose",
+    "Daffodil",
+    "Iris",
+    "Daisy",
+    "Tulip",
+    "Marigold",
+    "Lotus"
+  ];
+  console.log(flowerlist);
+  console.log(flowerlist[0]);
+  console.log(flowerlist[1]);
+  console.log(flowerlist[2]);
+  console.log(flowerlist[3]);
+  console.log(flowerlist[4]);
+  console.log(flowerlist[5]);
+  console.log(flowerlist[6]);
+  console.log(flowerlist[7]);
+  
+  console.log(flowerlist.length);
