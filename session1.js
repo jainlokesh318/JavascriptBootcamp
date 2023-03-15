262c446d5ee03964f6d49bf51c14979d97efd2f3
Lokesh Jain <lokesh.jain@juspay.in>
2023-03-15 21:39:15 +0530

    boolean intro

--- session1.js
+++ session1.js
@@ -7,9 +7,9 @@ let num2 = 48
 let sum = num + num2
 console.log(sum)
 
-const test = 4;   //declaration and initialization
-console.log(test)
-//test = 5   -> cannot change value of a constant
+const erg = 4 //declaration and initialization
+console.log(erg)
+//erg = 5  // -> cannot change value of a constant
 
 
 
@@ -18,3 +18,10 @@ let text = "text1"
 // console.log(printName)
 // console.log(printName)
 // console.log(printName)
+
+//declare a variable myVariable and initialize it to value "testing123"
+//then reinitialize it to "random123"
+let myVariable = "testing123"
+console.log(myVariable)
+myVariable = "random123"
+console.log(myVariable)
