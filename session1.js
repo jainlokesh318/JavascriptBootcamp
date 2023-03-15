ba3a14d03d79418ca9fde918353e6bf738c4604f
Lokesh Jain <lokesh.jain@juspay.in>
2023-03-15 21:08:15 +0530

    const value intro

--- session1.js
+++ session1.js
@@ -2,11 +2,15 @@ let num  // --------> declaration
 num = 35 // --------> initialization
 num = 48 // --------> re-initialization
 
-
 let num2 = 48
 
 let sum = num + num2
 console.log(sum)
+
+const test = 4;   //declaration and initialization
+console.log(test)
+//test = 5   -> cannot change value of a constant
+
 
 
 let text = "text1"
