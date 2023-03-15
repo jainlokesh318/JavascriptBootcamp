8a0d2764e7ea77ca333bb619a62b411d0473dd62
Lokesh Jain <lokesh.jain@juspay.in>
2023-03-15 20:58:00 +0530

    variable initialization & declaration

--- session1.js
+++ session1.js
@@ -1,4 +1,16 @@
-let printName = "Javascript2"
-console.log(printName)
-console.log(printName)
-console.log(printName)
+let num  // --------> declaration
+num = 35 // --------> initialization
+num = 48 // --------> re-initialization
+
+
+let num2 = 48
+
+let sum = num + num2
+console.log(sum)
+
+
+let text = "text1"
+
+// console.log(printName)
+// console.log(printName)
+// console.log(printName)
