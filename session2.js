99a72d1ed7f0a5149f5bc2405f06fe02ee1cefb8
Lokesh Jain <lokesh.jain@juspay.in>
2023-03-17 20:45:06 +0530

    functions reference

---
+++ session2.js
@@ -0,0 +1,31 @@
+// console.log("UTHOOOO");
+// console.log("Remote k paas jao");
+// console.log("PRess green button");
+
+// console.log("UTHOOOO");
+// console.log("Remote k paas jao");
+// console.log("PRess green button");
+
+// console.log("UTHOOOO");
+// console.log("Remote k paas jao");
+// console.log("PRess green button");
+
+// console.log("UTHOOOO");
+// console.log("Remote k paas jao");
+// console.log("PRess green button");
+//--------------------------------------------
+
+//ALternatively we can use functions 
+//function declaration
+function turnOnTV() {
+    console.log("UTHOOOO");
+    console.log("Remote k paas jao");
+    console.log("PRess green button");
+}
+
+//Reference to the function
+turnOnTV
+
+//function calling / invokation
+turnOnTV()
+turnOnTV()
