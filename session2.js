5de3c0be27e1ef83b796f558601c585d4ea132dc
Lokesh Jain <lokesh.jain@juspay.in>
2023-03-18 17:45:43 +0530

    function arguments intro

--- session2.js
+++ session2.js
@@ -30,7 +30,63 @@ turnOnTV
 turnOnTV()
 turnOnTV()
 
+//Exercise Functions
 //consider you have 30 marks till now
 //after that for 5 next questions whoose status we will know by a variable, 
 //update the totalMarks using a function
 //at the end print the totalMarks
+
+let totalMarks = 30;
+
+function updateMarks(){
+    if(isCorrect){
+        totalMarks = totalMarks + 5;
+    }else{
+        totalMarks = totalMarks - 1;
+    }
+    console.log("Updated Marks", totalMarks)
+}
+
+let isCorrect = true;
+updateMarks();
+
+isCorrect = false;
+updateMarks();
+
+isCorrect = true;
+updateMarks()
+
+isCorrect = false;
+updateMarks();
+
+
+function allooKiSubji(){
+    console.log("wash alloo");
+    console.log("put on flame");
+    console.log("cook alloo");
+}
+
+
+function bhindiKiSubji(){
+    console.log("wash bhindi");
+    console.log("put on flame");
+    console.log("cook bhindi");
+}
+
+function karelaKiSubji(){
+    console.log("wash karela");
+    console.log("put on flame");
+    console.log("cook karela");
+}
+
+
+//arguments
+function cook(subji){
+    console.log("wash", subji);
+    console.log("put on flame");
+    console.log("cook on flame", subji);
+}
+
+cook("alloo")
+cook("bhindi")
+cook("karela")
