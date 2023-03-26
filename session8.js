3ed3470d698b2c3c30cdbf9720b87159ed693245
Lokesh Jain <lokesh.jain@juspay.in>
2023-03-26 17:37:08 +0530

    exercise added

--- session8.js
+++ session8.js
@@ -1,6 +1,6 @@
 let school = {
     name: "Marian School",
-    class: 10
+    class: 10,
 }
 console.log(school)
 school["address"] = "Uttarakhand"
@@ -10,3 +10,14 @@ console.log("Super After", school)
 
 //Exercise 1
 //Create a object with 2 keys in it and later add two more keys as well as update the two existing keys
+
+//for in loop
+for(let key in school){
+    console.log(key);
+    console.log(school[key]);
+}
+
+let key = "class"
+console.log(school[key])
+
+
