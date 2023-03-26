c1e7ab21a109e5c310bcf491dfdf08306bcdf872
Lokesh Jain <lokesh.jain@juspay.in>
2023-03-26 17:17:22 +0530

    exercise added

--- session8.js
+++ session8.js
@@ -7,3 +7,6 @@ school["address"] = "Uttarakhand"
 console.log("After", school)
 school["class"] = 12
 console.log("Super After", school)
+
+//Exercise 1
+//Create a object with 2 keys in it and later add two more keys as well as update the two existing keys
