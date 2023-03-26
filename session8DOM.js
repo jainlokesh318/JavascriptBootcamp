f3b8b1991601ad98a8851a089f78823cb3a072cd
Lokesh Jain <lokesh.jain@juspay.in>
2023-03-26 19:08:19 +0530

    DOM intro

---
+++ session8DOM.js
@@ -0,0 +1,15 @@
+console.log("DOM is being understood")
+
+let heading = document.getElementById("mainHeading")
+//console.log(heading)
+
+//TODO :- Read about them
+//innerHTML
+//innerText
+//outerText
+//textContent
+
+console.log(heading["innerText"])
+console.log(heading["innerHTML"])
+console.log(heading["outerText"])
+console.log(heading["textContent"])
