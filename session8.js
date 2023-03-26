44f88ee711af19ea12e606e9abf565237f8af74d
Lokesh Jain <lokesh.jain@juspay.in>
2023-03-26 17:15:46 +0530

    objects property set intro

---
+++ session8.js
@@ -0,0 +1,9 @@
+let school = {
+    name: "Marian School",
+    class: 10
+}
+console.log(school)
+school["address"] = "Uttarakhand"
+console.log("After", school)
+school["class"] = 12
+console.log("Super After", school)
