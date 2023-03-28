5362d8b964aa09f5c6548948d471f091e47b0537
Lokesh Jain <lokesh.jain@juspay.in>
2023-03-28 23:27:48 +0530

    adding exercises

--- Array&ObjectsExercises.js
+++ Array&ObjectsExercises.js
@@ -5,7 +5,14 @@
 //Write a function that merges two given arrays and returns the merged array.
 
 //Exercise 3
-//Write a function that merges two given objects and returns the merged object.
+//Write a function that returns a object with the evenSum and oddSum of given array of numbers, 
+
+//Given: [1, 2, 3, 4, 5]
+//Return
+// {
+//   evenSum: 6 (2+4)
+//   oddSum:  9 (1+3+5)
+// } 
 
 //Exercise 4
 // let names = ["Karishma", "Krishna", "Ram", "Shyam", .......]
@@ -29,3 +36,24 @@
 //     }
 //     and so on... 
 // ] 
+
+//Exercise 5
+// Given a object of sweets and their sweetness level.
+// Write a function that returns sweets objects above 100 sweetness level 
+//
+// Given : 
+// [
+//     { name: "halwa", sweetness:10 },
+//     { name: "boondi", sweetness:50 },
+//     { name: "gulabJamun", sweetness:120 },
+//     { name: "rasmalai", sweetness:200 },
+//     { name: "ghewar", sweetness:100 },
+//     ...so on
+// ]
+
+// Result:
+// [
+//     { name: "gulabJamun", sweetness:120 },
+//     { name: "rasmalai", sweetness:200 },
+//     ...so on
+// ]
