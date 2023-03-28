3be91ccbc3fe2c3f507e3026edee9a1fc77d4eb6
Lokesh Jain <lokesh.jain@juspay.in>
2023-03-28 23:11:16 +0530

    adding exercises

--- Array&ObjectsExercises.js
+++ Array&ObjectsExercises.js
@@ -4,6 +4,28 @@
 //Exercise 2
 //Write a function that merges two given arrays and returns the merged array.
 
-//Exercise 2
+//Exercise 3
 //Write a function that merges two given objects and returns the merged object.
 
+//Exercise 4
+// let names = ["Karishma", "Krishna", "Ram", "Shyam", .......]
+// let ages = [10, 20, 55, 30, .......]
+//
+// Write a function that takes names, and their respective ages and returns a array of objects where each object has respective name and age
+//
+// Result :  
+// [
+//     {
+//         name: "Karishma",
+//         age: 10
+//     },
+//     {
+//         name: "Krishna",
+//         age: 20
+//     },
+//     {
+//         name: "Ram",
+//         age: 55
+//     }
+//     and so on... 
+// ] 
