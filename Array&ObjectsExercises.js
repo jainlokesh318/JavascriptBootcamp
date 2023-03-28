79040073fcb174813843b76820e1bdf4fe7fb71c
Lokesh Jain <lokesh.jain@juspay.in>
2023-03-28 23:00:12 +0530

    adding exercised

--- Array&ObjectsExercises.js
+++ Array&ObjectsExercises.js
@@ -1,2 +1,9 @@
 //Exercise 1
 //Write a function that returns a newCopy of given two objects with all their properties in the single new Object
+
+//Exercise 2
+//Write a function that merges two given arrays and returns the merged array.
+
+//Exercise 2
+//Write a function that merges two given objects and returns the merged object.
+
