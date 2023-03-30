2dcb19fced94bb312271a4e36b125887e33b5553
Lokesh Jain <lokesh.jain@juspay.in>
2023-03-30 08:58:03 +0530

    innerText vs innerHTML

--- session9.js
+++ session9.js
@@ -1,5 +1,6 @@
-let myHeading = document.getElementById("heading");
-console.log(myHeading.innerText);
+// let myHeading = document.getElementById("heading");
+// console.log(myHeading.innerText);
+
 // const other = document.getElementById("xyz");
 // console.log(other)
 // //other.innerText would give error if id not in html
@@ -7,13 +8,39 @@ console.log(myHeading.innerText);
 // myHeading.innerText = myHeading.innerText + "Lokesh"
 // // Hello + Lokesh
 
-let kid = document.getElementById("bache");
-console.log(kid)
-
-let buttonClick = () => {
-    console.log("Hello")
-}
-
-//Adding ears to the kid
-kid.addEventListener("click", buttonClick)
+// let kid = document.getElementById("bache");
+// console.log(kid)
+// let buttonClick = () => {
+//     console.log("Hello")
+// }
+// kid.addEventListener("click", buttonClick)
 //Note:- not function call, but function is passed
+
+//Exercise 1
+//Print your name on click of a button
+
+
+//Exercise 2
+//Increment counter on button click
+
+// let myHeading = document.getElementById("heading");
+
+// let kid = document.getElementById("bache");
+// let count = 1;
+
+// myHeading.innerText = count;
+
+// let buttonClick = () => {
+//     count = count - 1;
+//     myHeading.innerText = count
+// }
+// kid.addEventListener("click", buttonClick)
+
+let division = document.getElementById("test")
+console.log(division.innerText)
+console.log(division.innerHTML)
+
+//division.innerText = 4
+division.innerHTML = "<button>Injected From JS</button>" + division.innerHTML
+//division.innerText = "<button>Injected From JS</button>"
+
