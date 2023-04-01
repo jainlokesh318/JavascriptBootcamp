0f18b73b5ae91c9175897121254e3612243fa658
Lokesh Jain <lokesh.jain@juspay.in>
2023-04-01 18:21:04 +0530

    todo exercise added

--- session10.js
+++ session10.js
@@ -26,7 +26,7 @@ const printStr1 = () => {
 const printName1 = (cb) => {
     cb()
 }
-printName1(printStr)
+printName1(printStr1)
 
 //Exercise 2
 //Write a function that prints given name using a callback.
@@ -43,11 +43,34 @@ printName("Lokesh", printStr)
 //Write a function which takes 3 arguments :- 2callback, and one number
 //If number is odd then call the first callback
 //Else call the second callback
+const oddCall = () => {
+    console.log("Odd number here")
+}
+
+const evenCall = () => {
+    console.log("Even number here")
+}
+
+const deciding = (cb1, cb2, num) => {
+    if(num%2 === 0){
+        cb1()
+    }else{
+        cb2()
+    }
+}
+
+deciding(evenCall, oddCall, 5)
+deciding(evenCall, oddCall, 4)
 
 
-//Exercise 4
+//TODO: Exercise 4
+//Write a function which takes 3 arguments :- 2callback, and one number
+//If number is odd then call the first callback which prints that number also
+//Else call the second callback which prints that number also
+
+
+//TODO:- Exercise 5
 //Through a variable you will know if its raining or not
-//
 //Write a function which calls the makePakoda function if its raining
 //else it calls the bringIceCream function
 //Note: The functions are passed as callback 
