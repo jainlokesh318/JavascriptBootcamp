5fa0d823e6bc7e2a6b38d43ce719533b8d32e34b
Lokesh Jain <lokesh.jain@juspay.in>
2023-04-12 17:52:46 +0530

    hoisting and temporal dead zone

---
+++ es6.js
@@ -0,0 +1,15 @@
+//var, let, const
+console.log(a)
+var a;
+console.log(a) 
+a = 2;
+console.log(a)
+
+abc();
+//Function declaration
+function abc(){
+    console.log("Hello function called")
+}
+
+
+
