7f879084f12172ad4bf25c653594cc54bf7b41ad
Lokesh Jain <lokesh.jain@juspay.in>
2023-04-12 19:14:42 +0530

    const with objects

--- es6.js
+++ es6.js
@@ -43,19 +43,51 @@
 // }
 // console.log("2",a);
 
-let a = 10;
-function xyz(){
-    console.log("before", a)
-    var a = 4;
-    console.log("1",a)
-    {
-        a = 8
-        console.log("2",a)
+// let a = 10;
+// function xyz(){
+//     console.log("before", a)
+//     var a = 4;
+//     console.log("1",a)
+//     {
+//         a = 8
+//         console.log("2",a)
+//     }
+//     console.log("3",a)
+// }
+// console.log("4",a);
+// xyz()
+
+
+// let r;
+// console.log(r);
+
+// r = 10;
+// console.log(r);
+
+// let r = 10;
+// console.log(r);
+// r = 7;
+// console.log(r);
+
+// const r = 10;
+// console.log(r);
+// r = 9;
+// console.log(r);
+
+const person = {
+    firstName: "Ram",
+    lastName: "Gupta",
+    address: {
+        country: "India",
+        state: "MP",
+        district: "Sagar"
     }
-    console.log("3",a)
 }
-console.log("4",a);
-xyz()
+person.firstName = "Shyam"
+
+person.address.pincode = 123456
+
+console.log(person)
 
 //TODO :- 
 //const with objects
